f4f1951135e074ef1aae1d210ba5d4aa6fa1fd20
Israr A. <91403838+Israr-11@users.noreply.github.com>
2025-07-01 06:51:10 +0100

    Update _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,4 +1,5 @@
 import { Tabs } from "expo-router";
+
 import { useColorScheme } from "react-native";
 import { Colors } from "../../constants/Colors";
 import { Ionicons } from "@expo/vector-icons";
